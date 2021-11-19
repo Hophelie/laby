f15c4b04a519929aa4c943dee5161ec4a4c34f88
Mehdi <agounine.mehdi@gmail.com>
2021-11-19 15:49:41 +0100

    les murs des cases sont fait avec des classe css/ la fonction move brise les murs

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
 const container = document.querySelector('#container');
 
-let length = 25
-let width = 25
-let personnage1 = {
+const width = 4
+const height = 4
+const personnage1 = {
     coord: {
-        x: 5,
+        x: 0,
         y: 0
     },
     color: 'red'
 }
-let personnage2 = {
+const personnage2 = {
     coord: {
-        x: 5,
-        y: 24
+        x: 4,
+        y: 4
     },
     color: 'blue'
 }
 
 // genère les cases du tableau en html
 function generateMap(){
-    for (let i = 0; i<length; i++){
-        container.insertAdjacentHTML('beforeend', `<div id="row-${i}" class="row"></div>`)
+    for (let i = 0; i<height; i++){
+        container.insertAdjacentHTML('beforeend', `<tr id="row-${i}" class="row"></tr>`)
         for (let j =0; j<width; j++){
             let rowEnCours = document.querySelector(`#row-${i}`);
-            rowEnCours.insertAdjacentHTML('beforeend', `<div id="case-${i}-${j}" class="case"></div>`)
+            rowEnCours.insertAdjacentHTML('beforeend', `<td id="case-${i}-${j}" class="case bt br bb bl"></td>`)
         }
     }
 }
@@ -72,23 +72,38 @@ function updatePosition(event){
 function move(personnage,direction){
 
     const lastCoord = personnage.coord
+    const {x: lastX, y: lastY} = lastCoord;
+    lastCase = document.querySelector(`#case-${lastY}-${lastX}`)
+    
     switch (direction){
         case 'haut':
+            nextCase = document.querySelector(`#case-${lastY - 1}-${lastX}`)
+            lastCase.classList.remove('bt')
+            nextCase.classList.remove('bb')
             clear(lastCoord);
             personnage.coord.y > 0 ? personnage.coord.y-- : ''
             spawn(personnage)
             break
         case 'bas':
+            nextCase = document.querySelector(`#case-${lastY + 1}-${lastX}`)
+            lastCase.classList.remove('bb')
+            nextCase.classList.remove('bt')
             clear(lastCoord);
-            personnage.coord.y < 24 ? personnage.coord.y++ : ''
+            personnage.coord.y < height - 1 ? personnage.coord.y++ : ''
             spawn(personnage)
             break
         case 'droite':
+            nextCase = document.querySelector(`#case-${lastY}-${lastX + 1}`)
+            lastCase.classList.remove('br')
+            nextCase.classList.remove('bl')
             clear(lastCoord);
-            personnage.coord.x < 24 ? personnage.coord.x++ : ''
+            personnage.coord.x < width - 1 ? personnage.coord.x++ : ''
             spawn(personnage)
             break
         case 'gauche':
+            nextCase = document.querySelector(`#case-${lastY}-${lastX - 1}`)
+            lastCase.classList.remove('bl')
+            nextCase.classList.remove('br')
             clear(lastCoord);
             personnage.coord.x > 0 ? personnage.coord.x-- : ''
             spawn(personnage)
